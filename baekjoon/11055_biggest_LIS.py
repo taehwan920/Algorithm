b1598a4324dfad52df2ce7763735f7a1c6a46911
Yang Taehwan <taehwan920@gmail.com>
2020-05-19 20:22:37 +0900

    baekjoon 11055 biggest_LIS py

---
+++ baekjoon/11055_biggest_LIS.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+nums = list(map(int, input().split()))
+dp = [i for i in nums]
+
+for i in range(n):
+    for j in range(i):
+        if nums[j] < nums[i]:
+            dp[i] = max(dp[i], dp[j] + nums[i])
+
+print(max(dp))
