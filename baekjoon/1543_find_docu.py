ac89c753242c331ea75c02e4b0027fe1ae231ff3
Yang Taehwan <taehwan920@gmail.com>
2020-05-28 10:37:31 +0900

    baekjoon 1543 find docu again py

--- baekjoon/1543_find_docu.py
+++ baekjoon/1543_find_docu.py
@@ -13,3 +13,22 @@ while len(docu) - index >= len(word):
         index += 1
 
 print(result)
+
+# 밑엔 한번 더 풀었던 것. for문의 경우 i가 자동으로 1씩 늘어나기때문에 찾고자하는 단어의 길이에서 1을 빼준값을 더해줘야 했었다. 이걸 안해줘서 계속 틀렸다는 메세지가 나옴.
+# 이런류의 문제 풀땐 주의해야할 것.
+source = input()
+that = input()
+cnt = 0
+jump = 0
+
+for i in range(len(source)):
+    if len(source) - i < len(that):
+        break
+    if jump:
+        jump -= 1
+        continue
+    if source[i:i + len(that)] == that:
+        jump += len(that)-1
+        cnt += 1
+
+print(cnt)
