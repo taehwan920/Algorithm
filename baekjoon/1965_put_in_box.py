2f5623ab9005ed6e9414412c35fa3bb2beb9dcd1
Yang Taehwan <taehwan920@gmail.com>
2020-05-29 20:05:55 +0900

    baekjoon 1965 put in box

---
+++ baekjoon/1965_put_in_box.py
@@ -0,0 +1,10 @@
+n = int(input())
+boxes = list(map(int, input().split()))
+dp = [1 for i in range(n)]
+
+for i in range(1, n):
+    for j in range(i):
+        if boxes[j] < boxes[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
