1b78c48c40094d77b1b70f1fd7c767524b3c2dca
Yang Taehwan <taehwan920@gmail.com>
2020-06-18 11:17:01 +0900

    programmers level3 등굣길 py

--- programmers/way_to_school.py
+++ programmers/way_to_school.py
@@ -25,3 +25,27 @@ def solution(m, n, puddles):
 
     return routes[-1][-1] % 1000000007
 # 동적계획법의 과정중에 완전탐색이 있을 뿐
+
+
+def solution1(m, n, puddles):
+    dp = [[1 for j in range(m)] for i in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if [j+1, i+1] in puddles:
+                if i == 0:
+                    for k in range(j, m):
+                        dp[i][k] = 0
+                elif j == 0:
+                    for k in range(i, n):
+                        dp[k][j] = 0
+                else:
+                    dp[i][j] = 0
+                continue
+            if i == 0 or j == 0:
+                continue
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    return dp[-1][-1] % 1000000007
+
+# 전에 풀었던 건 거의 단순 구현 식이라 이번엔 좀 더 dp스럽게 풀어보았다.
